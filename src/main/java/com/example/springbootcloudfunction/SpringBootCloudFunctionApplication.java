673c93295c80dbc813828baa6c1e3e271dbc73d6
melsaied <m.elsaied@gmail.com>
2023-07-05 19:38:45 -0500

    first commit

---
+++ src/main/java/com/example/springbootcloudfunction/SpringBootCloudFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudFunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCloudFunctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootcloudfunction/SpringBootCloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudFunctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
