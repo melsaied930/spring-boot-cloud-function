7f41a566a549c29dbeb340e89d557f87af4e7d42
melsaied <m.elsaied@gmail.com>
2023-09-06 17:13:47 -0500

    om.example.springboot.cloud.functions

--- src/test/java/com/example/springboot/cloud/functions/SpringBootCloudFunctionApplicationTests.java
+++ src/test/java/com/example/springboot/cloud/functions/SpringBootCloudFunctionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springbootcloudfunction;
+package com.example.springboot.cloud.functions;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootCloudFunctionApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
